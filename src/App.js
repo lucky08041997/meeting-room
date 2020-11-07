de5b732e7210524b23c54a96eedb966593a65aef
lucky08041997 <luckycahyahandika@gmail.com>
2020-11-07 16:34:13 +0700

    return to AdminPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import AdminPage from './pages/AdminPage';
-import UserPage from './components/user_page/userPage.jsx'
 
 
 function App() {
   return (
-    // <AdminPage />
-    <UserPage />
+    <AdminPage />
   );
 }
 
